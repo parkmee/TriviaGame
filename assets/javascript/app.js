ab4ddf69aa2e1beb79f621c630d359e59410138b
MMPark <parkmee@gmail.com>
2018-09-17 00:30:53 -0400

    finished game logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,6 @@
+$(document).ready(function() {
 // create question bank with question, answer values, correct answer, and 2 hints
+
 const questionBank = [
     {
         question: "What country flies the world's only national flag that consists solely of four horizontal color bands?",
@@ -29,9 +31,10 @@ const questionBank = [
 ]
 
 // setup game variables
-let userInput = [];
-let score = 0;
-let timer = 0;
+let correct = 0;
+let incorrect = 0;
+let unanswered = 0;
+let timer = 60;
 let timerInterval;
 
 // establish references to document
@@ -40,54 +43,36 @@ let gameMsg = $("#game-msg");
 let startBtn = $("#start-btn");
 let questionView = $(".question-view");
 let scoreView = $(".score-view");
+let submitBtn = $("#submit-btn");
 
 // on click of start button, start game
 startBtn.on("click", function() {
-    console.log("Hi");
+    
+    // hide start button
+    startBtn.hide();
+
+    // show submit button
+    submitBtn.show();
 
     // reset and start timer;
     startTimer();
 
-    // populate userInput array with length equal to selected question array
-    userInput.length = questionBank.length;
-    console.log(userInput);
+    // hide score from previous game
+    scoreView.empty();
+
+    // set game message
+    gameMsg.text("Think fast!");
 
     // populate questions and answers on screen
     showQuestions();
 });
 
-// on click of answer choice for question
-// replace null value of in userInput array with answer choice
-
 // give hints upon request
 // decrement score by quarter point each hint
 
-// if time runs out
-// tally score
-
-// if all answers are submitted
-// tally score
-
-// tallying score function
-// if userInput answer equals correct answer for that question, give point
-// calculate percentage correct
-// show start button
-
-let x = 0;
-
-console.log(questionBank[x].values[1]);
-console.log(questionBank[x].values.length);
-console.log(questionBank[x].correctAnswer);
-
-function findAnswer (x) {
-    for (var i = 0; i < questionBank[x].values.length; i++) {
-        if (questionBank[x].values[i] === questionBank[x].correctAnswer) {
-        console.log(questionBank[x].values[i]);
-        }
-    }
-}
-
-findAnswer(1);
+submitBtn.on("click", function() {
+    tally();
+});
 
 function showQuestions() {
 
@@ -99,7 +84,6 @@ function showQuestions() {
 
         // set q to equal the question
         const q = questionBank[countQ].question;
-        console.log(q);
 
         // reset count for the index position of the values array
         let countV = 0;
@@ -112,6 +96,8 @@ function showQuestions() {
 
             // set v equal to the value
             const v = questionBank[countQ].values[countV];
+
+            // append each value to the question
             questionView.append(
                 `
                     <div class="form-check form-check-inline">
@@ -119,14 +105,8 @@ function showQuestions() {
                         <label class="form-check-label" for="q${countQ}-v${countV}"> ${v}</label>
                     </div>
                 `
-            )
-            
-
-            // <div class="form-check form-check-inline">
-            //    <input type="radio" id="q${count}-0" value=${questionBank[count].values[0]}>
-            //    <label for="q${count}-0">${questionBank[count].values[0]}</label>
+            )      
             countV++;
-            console.log(v);
         }
         countQ++;
     }
@@ -134,10 +114,9 @@ function showQuestions() {
 
 function startTimer() {
     timer = 60;
-    startBtn.animate({ opacity: "0"});
     clearInterval(timerInterval);
     timerInterval = setInterval(decrement, 1000);
-}
+};
 
 function decrement() {
     // decrease timer by 1
@@ -147,12 +126,72 @@ function decrement() {
 
     // when timer hits 0
     if (timer === 0) {
-        // end the game
+        // end the game and tally score
         tally();
-    }
-}
+    };
+};
+
+function stop() {
+    clearInterval(timerInterval);
+};
 
 function tally() {
+    // stop timer
+    stop();
 
-}
+    // reset score to 0;
+    correct = 0;
+    incorrect = 0;
+    unanswered = 0;
 
+    // reset count for index position of questionBank array
+    let countQ = 0;
+
+    // for every question
+    for (let i in questionBank) {
+
+        // set name of input to new value
+        let name = `q${countQ}-options`;
+
+        // store answer of checked value for that question
+        var answer = $("input[name="+name+"]:checked").val();
+        console.log(answer);
+        console.log(questionBank[countQ].correctAnswer);
+        if (answer == questionBank[countQ].correctAnswer) {
+            correct++;
+        } else if (answer == undefined) {
+            unanswered++;
+        }
+        else {
+            incorrect++;
+        }
+        countQ++;        
+    };
+    console.log(correct, incorrect);
+    showScore();
+};
+
+function showScore () {
+    questionView.empty();
+    submitBtn.hide();
+    startBtn.show();
+    if (correct/questionBank.length === 1) {
+        gameMsg.text("Perfect score! You'd make an excellent spy!");
+    } else if (correct/questionBank.length >= 0.8) {
+        gameMsg.text("Great job!");
+    } else {
+        gameMsg.text("Better luck next time!");
+    }
+    scoreView.append (
+        `
+            <h3>Correct: ${correct}</h3>
+            <h3>Incorrect: ${incorrect}</h3>
+            <h3>Unanswered: ${unanswered}</h3>
+            <h>You got ${(correct/questionBank.length*100).toFixed(0)}% right!</h4>
+        `
+    )
+
+};
+
+
+});
